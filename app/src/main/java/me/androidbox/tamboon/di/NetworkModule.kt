980373efa367fbbd8689b300e16b221b6ee118c2
Steve <stevekitkat@gmail.com>
2020-05-28 22:27:53 +0700

    adds networkModule and dagger dependence

---
+++ app/src/main/java/me/androidbox/tamboon/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package me.androidbox.tamboon.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import me.androidbox.tamboon.BuildConfig
+import me.androidbox.tamboon.R
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+
+@Module
+class NetworkModule {
+
+    @Reusable
+    @Provides
+    fun httpLoginInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+
+        loggingInterceptor.level = if(BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        }
+        else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+
+        return loggingInterceptor
+    }
+
+    @Reusable
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Named("BaseUrl")
+    @Reusable
+    @Provides
+    fun provideBaseUrl(context: Context): String =
+        context.getString(R.string.baseUrl)
+
+    @Reusable
+    @Provides
+    fun provideRetrofit(@Named("BaseUrl") baseUrl: String, okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+}
---
+++ app/src/main/java/me/androidbox/tamboon/di/TamboonApplication.kt
@@ -0,0 +1,10 @@
+package me.androidbox.tamboon.di
+
+import android.app.Application
+
+class TamboonApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
