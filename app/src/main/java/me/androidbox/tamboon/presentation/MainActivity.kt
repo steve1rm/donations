96289243efc47e71c09d034f4a240409412ecd81
Steve <stevekitkat@gmail.com>
2020-05-28 18:04:03 +0700

    Initial commit adding dependencies

---
+++ app/src/main/java/me/androidbox/tamboon/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.androidbox.tamboon.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.androidbox.tamboon.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
