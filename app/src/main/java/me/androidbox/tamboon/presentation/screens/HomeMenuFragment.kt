d274148a270b2e81c08ef6c5c2081165d5037e97
Steve <stevekitkat@gmail.com>
2020-06-11 11:30:42 +0700

    Removed code from HomeMenuFragment

--- app/src/main/java/me/androidbox/tamboon/presentation/screens/HomeMenuFragment.kt
+++ app/src/main/java/me/androidbox/tamboon/presentation/screens/HomeMenuFragment.kt
@@ -9,23 +9,10 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import me.androidbox.tamboon.R
 import me.androidbox.tamboon.databinding.FragmentHomemenuBinding
-import me.androidbox.tamboon.di.FragmentModule
-import me.androidbox.tamboon.di.TamboonApplication
-import me.androidbox.tamboon.di.TamboonApplicationComponent
-import me.androidbox.tamboon.presentation.viewmodels.TamboonViewModel
-import javax.inject.Inject
 
 class HomeMenuFragment : Fragment() {
 
-    @Inject
-    lateinit var tamboonViewModel: TamboonViewModel
-
     private lateinit var binding: FragmentHomemenuBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        injectDependencies()
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHomemenuBinding.inflate(inflater, container, false)
@@ -43,15 +30,5 @@ class HomeMenuFragment : Fragment() {
 
     private fun tapFetchCharities(navController: NavController) {
         navController.navigate(R.id.action_homeMenuFragment_to_loadingFragment)
-    }
-
-    private fun injectDependencies() {
-        getTamboonApplicationComponent()
-            .fragmentSubcomponent(FragmentModule(this@HomeMenuFragment))
-            .inject(this@HomeMenuFragment)
-    }
-
-    private fun getTamboonApplicationComponent(): TamboonApplicationComponent {
-        return (requireActivity().application as TamboonApplication).tamboonApplicationComponent
     }
 }
